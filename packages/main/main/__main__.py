45abef4da5c17a2f5b1dd0e862d82d9a43c380f9
Anton Gorshkov <antong@gmail.com>
2024-10-28 18:15:54 -0400

    Main returns random quote now

--- packages/main/main/__main__.py
+++ packages/main/main/__main__.py
@@ -1,7 +1,11 @@
+import requests
 def main(args):
+  response = requests.get('https://zenquotes.io/api/random')
+  data = response.json()[0]
+  quote = data['q'] + ' - ' + data['a']
   return {
     'body': {
       'response_type': 'in_channel2',
-      'text': 'just a piece of text'
+      'text': quote
     }
   }
